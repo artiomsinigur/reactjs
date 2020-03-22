5e864ca590de82700e69f857a34ed63578c630d2
Artiom <sinigur2006@gmail.com>
2020-03-21 22:06:55 -0400

    Challenge refactoring counter with state

---
+++ indecision-app/src/playground/counter-state.js
@@ -0,0 +1,60 @@
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.getRandom = this.getRandom.bind(this);
+        // Every state we want to track
+        this.state = {
+            count: 0,
+            random: 133512
+        }
+    }
+
+    getRandom() {
+        const generate = () => Math.floor(Math.random() * 100)
+        
+        this.setState((prevState) => {
+            return { random: prevState.random = generate() }
+        })
+        console.log(this.state)
+    }
+
+    handleAddOne() {
+        // Will not work. It change the object but not the state
+        // this.state.count = this.state.count + 1;
+
+        // This will
+        this.setState((prevState) => {
+            return { count: prevState.count + 1 }
+        })
+    }
+    
+    handleMinusOne() {
+        this.setState((prevState) => {
+            return { count: prevState.count - 1 }
+        })
+    }
+    
+    handleReset() {
+        this.setState((prevState) => {
+            return { count: prevState.count = 0 }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.getRandom}>Random</button>
+                <p>Random: {this.state.random}</p>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'))
