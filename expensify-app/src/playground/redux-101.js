4b29e4af516e9e8697f610fb01482b27b64ff622
Artiom <sinigur2006@gmail.com>
2020-03-29 15:29:30 -0400

    Subscribing and Dynamic Actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -3,12 +3,18 @@ import { createStore } from 'redux'
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
+            }
+        case 'SET':
+            return {
+                count: action.count
             }
         case 'RESET':
             return {
@@ -20,11 +26,32 @@ const store = createStore((state = { count: 0 }, action) => {
 })
 
 // Fetch store state
-console.log(store.getState())
+// console.log(store.getState())
+
+// subscribe() function gets called every time the state change
+// You can use subscribe() to update the UI in response to state changes.
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
 
 // Actions
-store.dispatch({ type: 'INCREMENT' })
-store.dispatch({ type: 'DECREMENT' })
-store.dispatch({ type: 'RESET' })
+// Important part is that you should not mutate the state object, but return a new object if the state changes.
+store.dispatch({ 
+    type: 'INCREMENT',
+    incrementBy: 5 
+})
 
-console.log(store.getState())
+// Call here to stop watching
+// unsubscribe()
+
+store.dispatch({ type: 'RESET' })
+store.dispatch({ type: 'DECREMENT' })
+store.dispatch({ 
+    type: 'DECREMENT',
+    decrementBy: 5
+})
+
+store.dispatch({
+    type: 'SET',
+    count: 100
+ })
+
+// console.log(store.getState())
