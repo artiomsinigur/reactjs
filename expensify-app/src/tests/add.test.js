1fdcd3ead8b1573ae3c40a1eedd97f460178d64b
Artiom <sinigur2006@gmail.com>
2020-04-04 13:11:35 -0400

    Test expense actions

--- expensify-app/src/tests/add.test.js
+++ expensify-app/src/tests/add.test.js
@@ -1,10 +1,5 @@
 const add = (a, b) => a + b
-const generateGreeting = (name) => `Hello ${name}`
 
 test('Should add two numbers', () => {
     expect(add(3, 5)).toBe(8)
-})
-
-test('Should display string Mark', () => {
-    expect(generateGreeting('Mark')).toBe('Hello Mark')
 })
