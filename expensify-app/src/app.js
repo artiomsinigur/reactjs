b2227177384cbdcf77944f850f8e4c4554ab12ca
Artiom <sinigur2006@gmail.com>
2020-04-10 14:35:03 -0400

    Comment test dispatch expenses

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -10,12 +10,12 @@ import 'normalize.css/normalize.css'
 import './styles/style.scss'
 
 const store = configureStore()
-store.dispatch(addExpense({ desc: 'Weater bill', amount: 595, createdAt: 1585976053643 }))
-store.dispatch(addExpense({ desc: 'Meal bill', amount: 1600, createdAt: 1586620800000 }))
-store.dispatch(addExpense({ desc: 'Gas bill', amount: 260, createdAt: 1587225600000 }))
+// store.dispatch(addExpense({ desc: 'Weater bill', amount: 595, createdAt: 1585976053643 }))
+// store.dispatch(addExpense({ desc: 'Meal bill', amount: 1600, createdAt: 1586620800000 }))
+// store.dispatch(addExpense({ desc: 'Gas bill', amount: 260, createdAt: 1587225600000 }))
 
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+// const state = store.getState()
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 
 // 1. Redux Provider makes the Redux store available to the rest of app
 const jsx = (
